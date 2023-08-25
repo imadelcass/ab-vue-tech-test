f9c0ccf5e56315c600c3865acdb5410d234b7ed8
imadelcass <imadelcass@gmail.com>
2023-08-25 19:53:55 +0100

    Email verification page

---
+++ src/views/EmailVerificate.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-2/4 mx-auto h-screen pt-20">
+    <p class="pb-5">Email Verification sent! Check your mail box.</p>
+    <el-button v-loading="loading" type="primary" @click="resend()">Resend Email</el-button>
+    <el-button v-loading="loading" type="primary" link @click="$router.push({ name: 'login' })"
+      >Login</el-button
+    >
+  </div>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
+
+const authStore = useAuthStore()
+const loading = ref(false)
+
+const resend = () => {
+  loading.value = true
+  authStore.emailVerificate().then(() => {
+    loading.value = false
+  })
+}
+</script>
